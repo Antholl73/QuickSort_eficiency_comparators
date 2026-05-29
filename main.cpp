b8f1968c81abbf06dfd23d21498d8ed33351280b
Antholl73 <anthonyavilyo@gmail.com>
2026-05-28 23:37:35 -0500

    Actualizando main.cpp para exportar tiempos

--- main.cpp
+++ main.cpp
@@ -1,44 +1,89 @@
 #include "clase_vector.hpp"
 #include "quick_sort.hpp"
+#include "utils.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 #include <iostream>
 
+using namespace std;
+using VectorInt = Vector<int>; // define el tipo de los vectores
+
 int main() {
-  std::cout << "=== PRUEBA CON ENTEROS (INT) ===" << std::endl;
-  // Creamos un vector de 6 posiciones
-  Vector<int> vec_int(6);
+  srand(time(nullptr));
 
-  // Lo llenamos con números desordenados
-  vec_int[0] = 42;
-  vec_int[1] = 7;
-  vec_int[2] = 15;
-  vec_int[3] = 89;
-  vec_int[4] = 23;
-  vec_int[5] = 4;
+  ofstream archivo_csv("tiempos_quickSort.csv");
+  archivo_csv << "ELEMENTOS, PIVOTE_INICIO, PIVOTE_FINAL, PIVOTE MEDIO, PIVOTE "
+                 "RANDOM, PIVOTE MEDIA DE 3\n";
 
-  std::cout << "Vector original:  " << vec_int;
+  ulong size = 1;
 
-  // Llamamos a tu función de ordenamiento
-  quick_sort(vec_int);
+  while (size <= 500001) {
+    VectorInt v01(size);
+    VectorInt v02;
+    VectorInt v03;
+    VectorInt v04;
+    VectorInt v05;
 
-  std::cout << "Vector ordenado:  " << vec_int;
-  std::cout << "--------------------------------" << std::endl;
+    fill_random(v01, 1, static_cast<int>(size * 10));
 
-  std::cout << "\n=== PRUEBA CON DECIMALES (FLOAT) ===" << std::endl;
-  // Creamos un vector de 5 posiciones
-  Vector<float> vec_float(5);
+    v05 = v04 = v03 = v02 = v01;
 
-  vec_float[0] = 3.14f;
-  vec_float[1] = 1.61f;
-  vec_float[2] = 9.81f;
-  vec_float[3] = 0.5f;
-  vec_float[4] = 2.71f;
+    // PIVOTE INICIO
+    auto inicio1 = chrono::high_resolution_clock::now();
 
-  std::cout << "Vector original:  " << vec_float;
+    quick_sort(v01, Less<int>(), Pivot01());
 
-  quick_sort(vec_float);
+    auto fin1 = chrono::high_resolution_clock::now();
 
-  std::cout << "Vector ordenado:  " << vec_float;
-  std::cout << "--------------------------------" << std::endl;
+    chrono::duration<double, milli> tiempo1 = fin1 - inicio1;
+
+    // PIVOTE FINAL
+    auto inicio2 = chrono::high_resolution_clock::now();
+
+    quick_sort(v02, Less<int>(), Pivot02());
+
+    auto fin2 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, milli> tiempo2 = fin2 - inicio2;
+
+    // PIVOTE MEDIO
+    auto inicio3 = chrono::high_resolution_clock::now();
+
+    quick_sort(v03, Less<int>(), Pivot03());
+
+    auto fin3 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, milli> tiempo3 = fin3 - inicio3;
+
+    // PIVOTE RANDOM
+    auto inicio4 = chrono::high_resolution_clock::now();
+
+    quick_sort(v04, Less<int>(), Pivot04());
+
+    auto fin4 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, milli> tiempo4 = fin4 - inicio4;
+
+    // PIVOTE MEDIA DE 3
+    auto inicio5 = chrono::high_resolution_clock::now();
+
+    quick_sort(v05, Less<int>(), Pivot05());
+
+    auto fin5 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, milli> tiempo5 = fin5 - inicio5;
+
+    archivo_csv << size << "," << tiempo1.count() << "," << tiempo2.count()
+                << "," << tiempo3.count() << "," << tiempo4.count() << ","
+                << tiempo5.count() << "\n";
+
+    size += 10000;
+  }
+
+  archivo_csv.close();
+  cout << "datos exportados :)";
 
   return 0;
 }

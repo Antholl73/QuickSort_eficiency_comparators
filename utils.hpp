1ad8b1a032688ec65ca25b5fa7b6ac68e23ef9e3
Antholl73 <anthonyavilyo@gmail.com>
2026-05-28 23:54:04 -0500

    agregada funcion para imprimir valores

--- utils.hpp
+++ utils.hpp
@@ -2,6 +2,8 @@
 #define VECTOR_UTILS_HPP
 
 #include "clase_vector.hpp"
+#include <iomanip>
+#include <iostream>
 #include <random>
 #include <type_traits> // Necesario para saber si T es int o float
 
@@ -30,6 +32,31 @@ void fill_random(Vector<T> &v, T min_val, T max_val) {
       v[i] = dis(gen);
     }
   }
+}
+
+// Imprime el encabezado de la tabla una sola vez
+inline void print_table_header() {
+  // Línea separadora
+  std::cout << std::string(85, '-') << "\n";
+
+  // Nombres de las columnas con anchos fijos
+  std::cout << std::left << std::setw(12) << "ELEMENTOS" << std::setw(14)
+            << "P. INICIO(ms)" << std::setw(14) << "P. FINAL(ms)"
+            << std::setw(14) << "P. MEDIO(ms)" << std::setw(14)
+            << "P. RANDOM(ms)" << std::setw(15) << "P. MEDIA DE 3(ms)" << "\n";
+
+  // Línea separadora
+  std::cout << std::string(85, '-') << "\n";
+}
+
+// Imprime una fila con los tiempos exactos en milisegundos
+inline void print_table_row(ulong size, double t1, double t2, double t3,
+                            double t4, double t5) {
+  // std::fixed y std::setprecision(4) aseguran que siempre se vean 4 decimales
+  std::cout << std::left << std::fixed << std::setprecision(4) << std::setw(12)
+            << size << std::setw(14) << t1 << std::setw(14) << t2
+            << std::setw(14) << t3 << std::setw(14) << t4 << std::setw(15) << t5
+            << "\n";
 }
 
 #endif // !VECTOR_UTILS_HPP
